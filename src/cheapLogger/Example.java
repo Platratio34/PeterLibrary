c53052c4f7596fa1f0c9a31ca9b144385de3b040
Peter Crall <pcrall04@gmail.com>
2022-03-15 12:44:17 -0700

    Improved documentation and comments for CheapLogger

--- src/cheapLogger/Example.java
+++ src/cheapLogger/Example.java
@@ -1,18 +1,33 @@
 package cheapLogger;
 
-//import cheapLogger.CheapLogger.LogEvent;
+// Import the package
+import cheapLogger.*;
 
 public class Example {
-		
+	
 	static CheapLogger logger;
-	static int t = 0;
+	private static int t = 0;
+	public static int t2 = 0;
 	
 	public static void main(String[] args) {
+		// Creat a new instace of the CheapLogger creating a new custom LogEvent
+		// The Log event will be called in a seprate thead so you do not need to wory about it holding the main thread
 		logger = new CheapLogger(new LogEvent() {
 			public String onLog() {
-				t++;
-				return "Log:"+t;
+				// Create and return a string that will be logged
+				// When creating the string you can use instance and static varibles from the object that the LogEvent is created in
+				// Public, Private, and protected varibels can all be read
+				// For instnace I am using t and t2 in creating the string
+				return "Log:"+t+", "+t2;
 			}
 		});
+		// Start the rest of your program here
+		while(true) {
+			t++;
+			if(t<0) {
+				t = 0;
+				t2++;
+			}
+		}
 	}
 }
--- src/cheapLogger/LogEvent.java
+++ src/cheapLogger/LogEvent.java
@@ -5,5 +5,9 @@ public class LogEvent {
 	public LogEvent() {
 		
 	}
+	/**
+	 * Called when a button is pressd when the CheapLogger is the active window <br>
+	 * @return The string to be Logged
+	 */
 	public String onLog() {return "Log;";}
 }
